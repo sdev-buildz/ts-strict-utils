cf9ff0b0d118801c969fb4a5002e2f977cde1232
sdev-buildz <310740165+sdev-buildz@users.noreply.github.com>
2026-08-26 12:25:26 +0530

    tsconfig references for src and test. tsdown ignores tsconfig

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -8,12 +8,14 @@ export default defineConfig({
   // Output both ES Modules and CommonJS formats
   format: ['esm', 'cjs'],
   outDir: 'dist',
-  // Automatically generate .d.ts and .d.cts files
-  dts: true,
-
+  dts: {
+    build: false,
+  },
   // Clean the dist directory before building
   clean: true,
-  // tsconfig: 'tsconfig.build.json',
   // Optional: Generate source maps for debugging
   sourcemap: true,
+  treeshake: true,
+  minify: true,
+  tsconfig: false,
 })
